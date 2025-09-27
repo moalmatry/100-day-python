7d4ac2e3e66284a51e2bb3ef664e03d9e762a58d
Mohamed Almatry <moalmatry2000@gmail.com>
2025-09-27 19:38:10 +0300

    feature: finish day one

---
+++ day-1/task.py
@@ -0,0 +1,23 @@
+# print("Hello World \nHello world!")
+# print(`"Hello" + " Mohamed Almatry")
+
+# print("Hello " + input("What is your name? ") + "!")
+
+# name = input("What is your name?")
+
+# name = "Jack"
+# print(len(name))
+# print(f"Hello {name}")
+
+
+# user = input("What is your name? ")
+# usernameLength = len(user)
+# print(f"Hello {user}")
+# print(f"Your name has {usernameLength} letters in it")
+
+
+# Day 1 Project
+userCity = input("What's name of the city you grew up in?\n")
+userPet = input("What's your pet's name?\n")
+print("Welcome to the Band Name Generator.")
+print(f"Your band name could be {userCity} {userPet}")
