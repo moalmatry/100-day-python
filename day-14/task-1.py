1f6ffc05b8f40a242d2c5ecc60800ac3d63debb4
Mohamed Almatry <moalmatry2000@gmail.com>
2025-10-13 21:27:19 +0300

    feature: finsh section

--- day-14/task-1.py
+++ day-14/task-1.py
@@ -1,48 +1,51 @@
 from game_data import data
 import random
+import os
+import art
 
 
 def check_answer(guess, a_follower, b_follower):
-    if a_follower > b_follower:
-        return guess == "a"
-    else:
-        return guess == "b"
+    return guess == "a" if a_follower > b_follower else guess == "b"
 
 
 def get_random_account():
     return data[random.randint(0, len(data) - 1)]
 
 
-# def paly_the_game():
-#     score = 0
-#     game_should_continue = True
-#     account_a = get_random_account()
-#     account_b = get_random_account()
+def clear():
+    os.system("cls" if os.name == "nt" else "clear")
 
-#     while game_should_continue:
-#         account_a = account_b
-#         account_b = get_random_account()
 
-#         while account_a == account_b:
-#             account_b = get_random_account()
+def paly_the_game():
+    score = 0
+    game_should_continue = True
+    account_a = get_random_account()
+    account_b = get_random_account()
 
-#         print(
-#             f"Compare A: {account_a['name']}, a {account_a['description']}, from {account_a['country']}."
-#         )
-#         print("vs")
-#         print(
-#             f"Against B: {account_b['name']}, a {account_b['description']}, from {account_b['country']}."
-#         )
+    while game_should_continue:
+        account_a = account_b
+        account_b = get_random_account()
+        while account_a["follower_count"] == account_b["follower_count"]:
+            account_b = get_random_account()
 
-#         guess = input("Who has more followers? Type 'A' or 'B': ").lower()
-#         a_follower_count = account_a["follower_count"]
-#         b_follower_count = account_b["follower_count"]
-#         is_correct = check_answer(guess, a_follower_count, b_follower_count)
-
-#         # clear()
-#         # print(logo)
-#         if is_correct:
-#             score += 1
+        print(
+            f"Compare A: {account_a['name']}, a {account_a['description']}, from {account_a['country']}."
+        )
+        print(
+            f"Against B: {account_b['name']}, a {account_b['description']}, from {account_b['country']}."
+        )
+        guess = input("Who has more followers? Type 'A' or 'B': ").lower()
+        a_follower_count = account_a["follower_count"]
+        b_follower_count = account_b["follower_count"]
+        is_correct = check_answer(guess, a_follower_count, b_follower_count)
+        clear()
+        print(art.logo)
+        if is_correct:
+            score += 1
+            print(f"You're right! Current score: {score}.")
+        else:
+            game_should_continue = False
+            print(f"Sorry, that's wrong. Final score: {score}.")
 
 
 paly_the_game()
